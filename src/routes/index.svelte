783edd62145b741f9f622b689d0d5b52c0513941
Anton Aksionau <ikari808@gmail.com>
2021-07-01 22:31:32 +0200

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<header class="page-header">Anton Aksionau</header>
+
+<main class="main">Hi</main>
+
+<style>
+  :global(*) {
+    box-sizing: border-box;
+    font-family: sans-serif;
+  }
+  .page-header {
+    min-height: 56px;
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    background: #000;
+    color: #fff;
+  }
+  .main {
+    display: flex;
+    padding: 10px;
+  }
+</style>
