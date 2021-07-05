5f6c189796775c8cfe54fc366eeae15341af6ed0
Anton Aksionau <ikari808@gmail.com>
2021-07-05 22:55:02 +0200

    add halfmoon styles

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,18 @@
+<script context="module">
+</script>
+
+<script lang="ts">
+  import "halfmoon/css/halfmoon-variables.min.css";
+</script>
+
 <header class="page-header">Anton Aksionau</header>
 
 <main class="main">Hi</main>
+<button class="btn btn-primary">Button</button>
+<div class="custom-switch">
+  <input type="checkbox" id="switch-1" value="" />
+  <label for="switch-1">Switch 1</label>
+</div>
 
 <style>
   :global(*) {
