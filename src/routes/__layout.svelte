7f66a6dcb1f9149178227002efb7950998ebf191
Anton Aksionau <ikari808@gmail.com>
2021-07-11 23:03:47 +0200

    add pages

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,23 +2,41 @@
 </script>
 
 <script lang="ts">
-  import "halfmoon/css/halfmoon-variables.min.css";
+  // import "halfmoon/css/halfmoon-variables.min.css";
 </script>
 
-<header class="page-header">Anton Aksionau</header>
+<div class="page-wrapper with-navbar">
+  <nav class="navbar">
+    <a href="/" class="navbar-brand"> Anton Aksionau </a>
+    <ul class="navbar-nav d-none d-md-flex">
+      <!-- d-none = display: none, d-md-flex = display: flex on medium screens and up (width > 768px) -->
+      <li class="nav-item active">
+        <a href="/blog" class="nav-link">Blog</a>
+      </li>
+      <li class="nav-item">
+        <a href="/projects" class="nav-link">Projects</a>
+      </li>
+    </ul>
+  </nav>
+  <!-- <header class="page-header"></header> -->
 
-<main class="container-fluid main">
-  <slot />
-</main>
+  <main class="content-wrapper">
+    <slot />
+  </main>
+</div>
 
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&display=swap");
-
+  @import "halfmoon/css/halfmoon-variables.min.css";
   :global(*) {
     box-sizing: border-box;
     font-family: "Lato", sans-serif;
   }
-  .page-header {
+  .navbar {
+    background: #000;
+    color: #fff;
+  }
+  /* .page-header {
     min-height: 56px;
     display: flex;
     align-items: center;
@@ -28,9 +46,5 @@
 
     font-weight: 700;
     font-size: 2rem;
-  }
-  .main {
-    /* display: flex; */
-    /* padding: 10px; */
-  }
+  } */
 </style>
