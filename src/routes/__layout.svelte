f94a3cafce79a6348cebe51710ae62565ac4091f
Anton Aksionau <ikari808@gmail.com>
2021-07-11 16:07:49 +0200

    add halfmoon, update layout

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,36 @@
+<script context="module">
+</script>
+
+<script lang="ts">
+  import "halfmoon/css/halfmoon-variables.min.css";
+</script>
+
+<header class="page-header">Anton Aksionau</header>
+
+<main class="container-fluid main">
+  <slot />
+</main>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Lato:wght@100;300;400;700&display=swap");
+
+  :global(*) {
+    box-sizing: border-box;
+    font-family: "Lato", sans-serif;
+  }
+  .page-header {
+    min-height: 56px;
+    display: flex;
+    align-items: center;
+    padding: 10px;
+    background: #000;
+    color: #fff;
+
+    font-weight: 700;
+    font-size: 2rem;
+  }
+  .main {
+    /* display: flex; */
+    /* padding: 10px; */
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,34 +1,73 @@
-<script context="module">
-</script>
-
 <script lang="ts">
-  import "halfmoon/css/halfmoon-variables.min.css";
+  import FaLinkedin from "svelte-icons/fa/FaLinkedin.svelte";
+  import FaTwitterSquare from "svelte-icons/fa/FaTwitterSquare.svelte";
 </script>
 
-<header class="page-header">Anton Aksionau</header>
-
-<main class="main">Hi</main>
-<button class="btn btn-primary">Button</button>
-<div class="custom-switch">
-  <input type="checkbox" id="switch-1" value="" />
-  <label for="switch-1">Switch 1</label>
-</div>
+<section class="content">
+  <div class="row">
+    <div class="col-6 photo-wrapper">
+      <div class="photo" />
+    </div>
+    <div class="col-6 about">
+      <h2 class="about-head">Hello! My name is Anton.</h2>
+      <p class="about-body">I'm a web developer.</p>
+    </div>
+  </div>
+  <div class="row text-center">
+    <div class="col">
+      <h2 class="font-size-24">Contacts</h2>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col contacts">
+      <div class="contact">
+        <a href="https://www.linkedin.com/in/anton-aksionau/" target="_blank">
+          <FaLinkedin />
+        </a>
+      </div>
+      <div class="contact">
+        <a href="https://twitter.com/AntonAksionau" target="_blank">
+          <FaTwitterSquare />
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
 
 <style>
-  :global(*) {
-    box-sizing: border-box;
-    font-family: sans-serif;
+  .photo-wrapper {
+    justify-content: center;
+    align-items: center;
+    display: flex;
   }
-  .page-header {
-    min-height: 56px;
+  .photo {
+    width: 200px;
+    height: 200px;
+    max-width: 200px;
+    max-height: 200px;
+    border-radius: 50%;
+    background: #f2f2f2;
+  }
+
+  .about-head {
+    font-size: 2.4rem;
+    font-weight: 700;
+  }
+  .about-body {
+    font-size: 1.6rem;
+  }
+  .contacts {
     display: flex;
     align-items: center;
-    padding: 10px;
-    background: #000;
-    color: #fff;
+    justify-content: center;
   }
-  .main {
-    display: flex;
-    padding: 10px;
+  .contact {
+    width: 32px;
+    height: 32px;
+    margin-left: 10px;
+    cursor: pointer;
+  }
+  .contact:first-child {
+    margin-left: 0;
   }
 </style>
