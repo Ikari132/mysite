4ed6f520f9c82ac03e5c94ef96ca8849a1e26135
Anton Aksionau <ikari808@gmail.com>
2021-07-12 23:16:50 +0200

    add cards on projects page

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -1,3 +1,75 @@
 <section class="content">
-    Projects
+  <h2 class="title">My projects</h2>
+
+  <div class="row">
+    <a
+      class="card w-350 mw-full p-0 d-flex project-card"
+      href="/projects/counter"
+    >
+      <div class="w-100 h-100 m-10 align-self-center">
+        <div
+          class="w-100 h-100 rounded d-flex align-items-center justify-content-center"
+          style="background-color: #5352ed;"
+        />
+      </div>
+      <div
+        class="flex-grow-1 overflow-y-hidden d-flex align-items-center position-relative h-120"
+      >
+        <div class="p-10 w-full m-auto">
+          <p
+            class="font-size-18 text-dark-lm text-light-dm m-0 mb-5 text-truncate font-weight-medium"
+          >
+            Working time counter
+          </p>
+          <p class="font-size-14 mt-5 mb-0">
+            Browser extension that helps you to track time
+          </p>
+        </div>
+      </div>
+    </a>
+
+    <a
+      class="card w-350 mw-full p-0 d-flex project-card"
+      href="/projects/counter"
+    >
+      <div class="w-100 h-100 m-10 align-self-center">
+        <div
+          class="w-100 h-100 rounded d-flex align-items-center justify-content-center"
+          style="background-color: #5352ed;"
+        />
+      </div>
+      <div
+        class="flex-grow-1 overflow-y-hidden d-flex align-items-center position-relative h-120"
+      >
+        <div class="p-10 w-full m-auto">
+          <p
+            class="font-size-18 text-dark-lm text-light-dm m-0 mb-5 text-truncate font-weight-medium"
+          >
+            Working time counter
+          </p>
+          <p class="font-size-14 mt-5 mb-0">
+            Browser extension that helps you to track time
+          </p>
+        </div>
+      </div>
+    </a>
+  </div>
 </section>
+
+<style>
+  .title {
+    font-size: 2.4rem;
+    font-weight: 700;
+  }
+  .project-card {
+    --content-and-card-spacing: 0;
+
+    margin-left: 10px;
+  }
+  .project-card:first-child {
+    margin-left: 0;
+  }
+  .project-card:hover {
+    text-decoration: none;
+  }
+</style>
