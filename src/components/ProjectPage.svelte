b978b0143bcc5b4ba0291fb911edf55fad2f4053
AnastasiaElf <anastasia.elf.17@gmail.com>
2022-01-19 17:02:15 +0100

    add project features for Twity

--- src/components/ProjectPage.svelte
+++ src/components/ProjectPage.svelte
@@ -8,6 +8,7 @@
     export let description: string;
     export let links: { href: string; type: "Chrome" | "Firefox" | "Safari" }[];
     export let versions: { version: string; date?: string; changes: { title: string; description: string }[] }[];
+    export let features: { title: string; description: string; image: string; imageShadow?: boolean }[];
 </script>
 
 <section>
@@ -47,6 +48,28 @@
         </div>
     </div>
 
+    <div>
+        {#each features as feature}
+            <div class="row my-5 feature-wrapper gap-4 gap-md-0">
+                <div
+                    class="feature-image-wrapper col-12 col-md-6 d-flex justify-content-center justify-content-md-start"
+                >
+                    <img
+                        class="feature-image{feature.imageShadow === false ? '' : ' feature-image-shadow'}"
+                        src={feature.image}
+                        alt="asdf"
+                    />
+                </div>
+                <div
+                    class="feature-description-wrapper col-12 col-md-6 d-flex flex-column justify-content-center text-center text-md-start"
+                >
+                    <h3 class="fw-bold mb-3 mb-md-5 lh-base">{feature.title}</h3>
+                    <h5 class="lh-base">{feature.description}</h5>
+                </div>
+            </div>
+        {/each}
+    </div>
+
     <div class="row mt-3 mb-5">
         <div class="col-12">
             <div class="card">
@@ -76,6 +99,43 @@
         background-image: var(--logo-image);
         background-size: 100%;
         background-repeat: no-repeat;
+    }
+
+    .feature-image {
+        width: 100%;
+        border-radius: 8px;
+    }
+
+    .feature-image-shadow {
+        box-shadow: 0 1px 4px 0 #0000003d;
+    }
+
+    @media (max-width: 768px) {
+        .feature-image {
+            max-width: 340px;
+        }
+    }
+
+    @media (min-width: 768px) {
+        .feature-wrapper:nth-child(2n) {
+            flex-direction: row-reverse;
+        }
+
+        .feature-wrapper:nth-child(2n) .feature-image-wrapper {
+            padding-left: 30px;
+        }
+
+        .feature-wrapper:nth-child(2n) .feature-description-wrapper {
+            padding-right: 30px;
+        }
+
+        .feature-wrapper:nth-child(2n + 1) .feature-image-wrapper {
+            padding-right: 30px;
+        }
+
+        .feature-wrapper:nth-child(2n + 1) .feature-description-wrapper {
+            padding-left: 30px;
+        }
     }
 
     .button-with-icon {
--- src/routes/projects/twity/index.svelte
+++ src/routes/projects/twity/index.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <svelte:head>
-    <MetaTagsProvider title="Twity" description="Extension for deleting Tweets" name="twity"/>
+    <MetaTagsProvider title="Twity" description="Extension for deleting Tweets" name="twity" />
 </svelte:head>
 
 <ProjectPage
@@ -16,4 +16,16 @@
         { href: "https://chrome.google.com/webstore/detail/twity/mnencakcmnofdmpgmklkknklikgpodoo", type: "Chrome" },
     ]}
     {versions}
+    features={[
+        {
+            title: "Delete your Tweets in a few clicks",
+            description: "It deletes Tweets from the most recent to earlier ones",
+            image: "/twity-posts-number.png",
+        },
+        {
+            title: "No permissions are needed",
+            description: "All our extension does is click buttons for you",
+            image: "/twity-open-profile.png",
+        },
+    ]}
 />
