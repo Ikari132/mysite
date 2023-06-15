8ce3a7f16e01da98a805c3c998ad9be23e452afa
Anton Aksionau <ikari808@gmail.com>
2023-06-15 11:52:28 +0200

    update sdimage logo

--- src/lib/projects/sdimage.ts
+++ src/lib/projects/sdimage.ts
@@ -1,5 +1,5 @@
 import logo from "$lib/images/sdimage.png?width=500&height=500&imgtool";
-import logoWebp from "$lib/images/sdimage.png?width=500&height=500&webp&imgtool";
+import logoWebp from "$lib/images/sdimage-logo.png?width=500&height=500&webp&imgtool";
 import { versions } from "$lib/versions/sdimage";
 
 import type { IProject } from ".";
